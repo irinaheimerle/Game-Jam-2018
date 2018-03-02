f7e96709f0aade4f66b2bcb106164fdef8caa574
Irina Heimerle <irinaheimerle@gmail.com>
2018-03-02 10:10:27 -0400

    Main Menu - Inital Set Up
    Hope this works 8)

---
+++ myMainMenuGameJam/Code/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+
+    //quit function
+    public void ExitOnClick() {
+        Application.Quit();
+    }
+}
