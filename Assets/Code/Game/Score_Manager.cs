0743a587396c60e9c7e5c6a3f5c6c66d209828f8
Jaclyn Staples <jaclynonacloud@gmail.com>
2018-03-02 04:29:51 -0400

    Added Basic Multiplier Functionality

---
+++ Assets/Code/Game/Score_Manager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score_Manager : MonoBehaviour
+{
+
+	public Multiplier testMultiplier;
+	public Transform multiplierContainer;
+
+	private static int _currScore;
+	private static List<Multiplier> _multipliers;
+
+	// Use this for initialization
+	void Start()
+	{
+		AddMultiplier(testMultiplier);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+
+	//TODO: Remove Test case later.
+	private void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(150, 0, 100, 100));
+		if (GUILayout.Button("Test Multiplier"))
+			AddMultiplier(testMultiplier);
+		GUILayout.EndArea();
+	}
+
+
+	/*--------------------------------------------------------------------------------METHODS-----------------------*/
+	public void AddMultiplier(Multiplier multiplier)
+	{
+		Debug.Log("ADD MULT");
+		//create a copy
+		Multiplier mult = multiplier.CreateMultiplier(multiplierContainer);
+		//add multiplier
+		_multipliers.Add(mult);
+		//listen to multiplier
+		mult.OnFinish += OnMultiplierFinish;
+
+		Debug.Log("START MULT");
+
+	}
+
+	/*--------------------------------------------------------------------------------ABSTRACTS---------------------*/
+	/*--------------------------------------------------------------------------------EVENTS------------------------*/
+	private void OnMultiplierFinish(Multiplier multiplier)
+	{
+		//remove the listener
+		multiplier.OnFinish -= OnMultiplierFinish;
+
+		//remove the multiplier
+		if (_multipliers.Contains(multiplier))
+			_multipliers.Remove(multiplier);
+
+		//destroy the object
+		Multiplier.DestroyImmediate(multiplier);
+
+		Debug.Log("END MULT");
+	}
+	/*--------------------------------------------------------------------------------OVERRIDES---------------------*/
+	/*--------------------------------------------------------------------------------GETTERS and SETTERS-----------*/
+	public static int CurrentScore { get { return _currScore; } }
+
+
+}
---
+++ Assets/Code/Game/Terrain_Manager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain_Manager : MonoBehaviour {
+
+	public enum State 
+	{
+		Full,
+		Mid,
+		Dead
+	}
+
+	private static Terrain_Manager _instance;
+
+	[SerializeField]
+	private GameObject _terrainFull;
+	[SerializeField]
+	private GameObject _terrainMid;
+	[SerializeField]
+	private GameObject _terrainDead;
+
+	private static State _currentState;
+	private static GameObject _currTerrain;
+
+	private void Awake()
+	{
+		_instance = this;
+	}
+
+	private void Start()
+	{
+		ChangeState (State.Full);
+	}
+
+
+	/*TODO: Remove test from end.*/
+	private void OnGUI()
+	{
+		GUILayout.BeginArea (new Rect (0, 50, 250, 450));
+		if (GUILayout.Button ("Change To Full Terrain"))
+			ChangeState (State.Full);
+		if (GUILayout.Button ("Change To Mid Terrain"))
+			ChangeState (State.Mid);
+		if (GUILayout.Button ("Change To Dead Terrain"))
+			ChangeState (State.Dead);
+		GUILayout.EndArea ();
+	}
+
+
+	/*--------------------------------------------------------------------------------METHODS-----------------------*/
+	/// <summary>
+	/// Changes the state of the terrain.
+	/// </summary>
+	/// <param name="state">State.</param>
+	public static void ChangeState(State state)
+	{
+		_currentState = state;
+		//turn off last terrain
+		if (_currTerrain != null)
+			_currTerrain.SetActive (false);
+
+
+		//set the new terrain
+		switch (_currentState) 
+		{
+		case State.Full:
+			_currTerrain = _instance._terrainFull;
+			break;
+		case State.Mid:
+			_currTerrain = _instance._terrainMid;
+			break;
+		case State.Dead:
+			_currTerrain = _instance._terrainDead;
+			break;
+		}
+
+		//show new terrain
+		_currTerrain.SetActive (true);
+	}
+	/*--------------------------------------------------------------------------------ABSTRACTS---------------------*/
+	/*--------------------------------------------------------------------------------EVENTS------------------------*/
+	/*--------------------------------------------------------------------------------OVERRIDES---------------------*/
+	/*--------------------------------------------------------------------------------GETTERS and SETTERS-----------*/
+
+
+}
