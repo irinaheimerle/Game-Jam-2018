6011134d1a7e04d5c1842e0017461c01c7c224c8
Ryan Oakley <yelkao85@gmail.com>
2018-03-03 12:37:51 -0400

    upate for austin

---
+++ Assets/Code/Components/Earth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour {
+
+	public ParticleSystem impactPart;
+
+	public void AsteroidHit(Vector3 pos){
+		ParticleSystem cloneImpact = ParticleSystem.Instantiate(impactPart);
+		cloneImpact.transform.position = pos;
+
+		GameObject.Destroy (impactPart);
+
+		cloneImpact.GetComponent<GameObject_Destroyer> ().DestroyMe ();
+			
+		
+
+	}
+}
