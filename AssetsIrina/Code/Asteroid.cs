e4d2477664c7f87893a8feaf84929121210547a6
Irina Heimerle <irinaheimerle@gmail.com>
2018-03-03 11:37:29 -0400

    Main Menu and Pause Menu

---
+++ AssetsIrina/Code/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+    public GameObject asteroid;
+
+	// Use this for initialization
+	void Awake () {
+        
+        float x = Random.Range(3.4f, 53.5f);
+        float y = Random.Range(250f, 251f);
+        float z = Random.Range(-215f, 275f);
+
+
+        transform.position = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float x = asteroid.transform.position.x;
+        float y = asteroid.transform.position.y;
+        float z = asteroid.transform.position.z;
+        asteroid.transform.position = new Vector3(x, y-0.5f, z);
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("I am here");
+        //GetComponent<Renderer>().enabled = false;
+        AsteroidManager.DestroyAsteroid(this);
+    }
+}
---
+++ AssetsIrina/Code/Game_Manager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manages Game States and time played.
+/// </summary>
+public class Game_Manager : MonoBehaviour {
+	public delegate void Game_Event();
+	public static Game_Event OnStartGame, OnPauseGame, OnEndGame;
+    public Text timeTxt;
+    public float countSeconds =  0;
+    public int countMin = 0;
+
+    public enum State
+	{
+		Play,
+		Pause,
+		Game_Over
+	}
+
+	private static Game_Manager _instance;
+	private static State _currentState;
+
+	private static float _totalTime;
+
+
+	private void Awake() {
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_currentState == State.Play)
+			_totalTime += Time.deltaTime;
+
+        UpdateTimerUI();
+
+		//Debug.Log (_totalTime);
+	}
+
+
+    //Update timer 
+    public void UpdateTimerUI() {
+        countSeconds += Time.deltaTime;
+        //timeTxt.text = countMin + "m" + countSeconds + "s";
+        //txtTimer.text = "hello";
+
+
+        if (countSeconds >= 60) {
+            countMin++;
+            countSeconds = 0;
+        }
+    }
+
+
+
+	//TODO: Remove on testing complete.
+	private void OnGUI()
+	{
+		if (!Debug_Manager.ShowDebugTools) return;
+
+		if (GUILayout.Button ("Start Game"))
+			StartGame ();
+		if (GUILayout.Button ("End Game"))
+			EndGame ();
+	}
+
+	/*--------------------------------------------------------------------------------METHODS-----------------------*/
+	public static void StartGame()
+	{
+		if (OnStartGame != null)
+			OnStartGame ();
+
+		_currentState = State.Play;
+    }
+
+	public static void PauseGame()
+	{
+		if (OnPauseGame != null)
+			OnPauseGame ();
+
+		_currentState = State.Pause;
+
+        Debug.Log("I'm actually working!");
+	}
+
+	public static void EndGame()
+	{
+		if (OnEndGame != null)
+			OnEndGame ();
+
+		_currentState = State.Game_Over;
+	}
+
+	/// <summary>
+	/// Changes the state of the game.
+	/// </summary>
+	/// <param name="state">State.</param>
+	public static void ChangeState(State state)
+	{
+		_currentState = state;
+	}
+	/*--------------------------------------------------------------------------------ABSTRACTS---------------------*/
+	/*--------------------------------------------------------------------------------EVENTS------------------------*/
+	/*--------------------------------------------------------------------------------OVERRIDES---------------------*/
+	/*--------------------------------------------------------------------------------GETTERS and SETTERS-----------*/
+	public static Game_Manager Instance { get { return _instance; } }
+	public static State CurrentState { get { return _currentState; } }
+
+}
